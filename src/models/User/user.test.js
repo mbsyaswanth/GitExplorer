1e6b539914aadb9dc39da868904c18c321ed616b
srikanth bhamidipalli <srikanthbhamidipalli.3@gmail.com>
2019-08-21 12:33:46 +0530

    [red state] should check for the setRepos functionality

--- src/models/User/user.test.js
+++ src/models/User/user.test.js
@@ -10,4 +10,13 @@ describe('user model test cases ', () => {
     expect(user.avatarUrl).toBe(avatarUrl);
     expect(user.repoUrl).toBe(repoUrl);
   });
+  it('should check for the setRepos functionality', () => {
+    const userName = 'username';
+    const avatarUrl = 'avatarUrl';
+    const repoUrl = 'repoUrl';
+    const user = new User(userName, avatarUrl, repoUrl);
+    expect(user.repos).toHaveLength(0);
+    user.setRepos();
+    expect(user.repos).toHaveLength(1);
+  });
 });
