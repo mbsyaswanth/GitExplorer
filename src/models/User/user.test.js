fc59d33586ccba7774135327363bbf9016fe8463
srikanth bhamidipalli <srikanthbhamidipalli.3@gmail.com>
2019-08-21 12:26:29 +0530

    [refactored state] should check for the user model functionality

--- src/models/User/user.test.js
+++ src/models/User/user.test.js
@@ -2,9 +2,12 @@ import User from '.';
 
 describe('user model test cases ', () => {
   it('should check for the user model functionality', () => {
-    const user = new User('username', 'avatarUrl', 'repoUrl');
-    expect(user.userName).toBe('username');
-    expect(user.avatarUrl).toBe('avatarUrl');
-    expect(user.repoUrl).toBe('repoUrl');
+    const userName = 'username';
+    const avatarUrl = 'avatarUrl';
+    const repoUrl = 'repoUrl';
+    const user = new User(userName, avatarUrl, repoUrl);
+    expect(user.userName).toBe(userName);
+    expect(user.avatarUrl).toBe(avatarUrl);
+    expect(user.repoUrl).toBe(repoUrl);
   });
 });
