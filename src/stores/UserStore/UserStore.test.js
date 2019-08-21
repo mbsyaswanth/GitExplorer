eacc21238dd0a1c9bc67379908f5764663031185
srikanth bhamidipalli <srikanthbhamidipalli.3@gmail.com>
2019-08-21 15:05:30 +0530

    [refactored state] should check for the setUsers functionality

--- src/stores/UserStore/UserStore.test.js
+++ src/stores/UserStore/UserStore.test.js
@@ -3,8 +3,9 @@ import UserServices from '../../services/UserServices/index.fixtures';
 describe('User store test cases', () => {
   it('should check for the setUsers functionality', () => {
     const store = new UserStore();
+    const services = new UserServices();
     expect(store.users).toHaveLength(0);
-    store.setUsers(new UserServices().getUsers());
+    store.setUsers(services.getUsers());
     expect(store.users).toHaveLength(2);
   });
 });
