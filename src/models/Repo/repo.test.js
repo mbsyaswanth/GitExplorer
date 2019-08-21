d4396ead39bda54f8b5de875e6f176e49203fdce
srikanth bhamidipalli <srikanthbhamidipalli.3@gmail.com>
2019-08-21 12:12:02 +0530

    [refactored state] should check for the repo model functionality

--- src/models/Repo/repo.test.js
+++ src/models/Repo/repo.test.js
@@ -2,8 +2,9 @@ import Repo from '.';
 
 describe('Repo model test cases', () => {
   it('should check for the repo model functionality', () => {
-    const repo = new Repo('repoName', 2, 3);
-    expect(repo.repoName).toBe('repoName');
+    const repoName = 'repoName';
+    const repo = new Repo(repoName, 2, 3);
+    expect(repo.repoName).toBe(repoName);
     expect(repo.stars).toBe(2);
     expect(repo.forks).toBe(3);
   });
