822af52356b2564a3e5e064a054b673f438d0769
srikanth bhamidipalli <srikanthbhamidipalli.3@gmail.com>
2019-08-21 14:42:18 +0530

    [red state] should check for the setUsers functionality

---
+++ src/stores/UserStore/UserStore.test.js
@@ -0,0 +1,10 @@
+import UserStore from './index';
+import UserServices from '../../services/UserServices/index.fixtures';
+describe('User store test cases', () => {
+  it('should check for the setUsers functionality', () => {
+    const store = new UserStore();
+    expect(store.users).toHaveLength(0);
+    store.setUsers(new UserServices.getUsers());
+    expect(store.users).toHaveLength(1);
+  });
+});
