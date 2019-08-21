7f73a0626ff21172a2cb889b975d6fe57a19e734
srikanth bhamidipalli <srikanthbhamidipalli.3@gmail.com>
2019-08-21 12:16:21 +0530

    [red state] should check for the user model functionality

---
+++ src/models/User/user.test.js
@@ -0,0 +1,8 @@
+describe('user model test cases ', () => {
+  it('should check for the user model functionality', () => {
+    const user = new User('username', 'avatarUrl', 'repoUrl');
+    expect(user.userName).toBe('username');
+    expect(user.avatarUrl).toBe('avatarUrl');
+    expect(user.repoUrl).toBe('repoUrl');
+  });
+});
