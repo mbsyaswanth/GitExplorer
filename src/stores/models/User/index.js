396442031bd767a7d0def04e0053a78c251a5add
srikanth bhamidipalli <srikanthbhamidipalli.3@gmail.com>
2019-08-22 14:51:19 +0530

    actions are added for setting apiStatus and ErrorMessage in user model

--- src/stores/models/User/index.js
+++ src/stores/models/User/index.js
@@ -14,23 +14,32 @@ class User {
     this.avatarUrl = avatarUrl;
     this.repoUrl = repoUrl;
     this.serviceName = serviceName;
+    this.errorMessage = '';
   }
   @action.bound setRepos(repoData) {
     this.repos = repoData.map(repo => {
       new Repo(repo.name, repo.stargazers_count, repo.forks);
     });
   }
-  getRepos = async () => {
-    this.apiStatus = apiStatus.loading;
+  @action.bound async getRepos() {
+    this.setApiStatus(apiStatus.loading);
     try {
       let response = await this.serviceName.getRepos(this.repoUrl);
       let reposData = await response.json();
       this.setRepos(reposData);
-      this.apiStatus = apiStatus.completed;
+      this.setApiStatus(apiStatus.completed);
     } catch (e) {
-      this.apiStatus = apiStatus.error;
+      this.setApiStatus(apiStatus.error);
+      this.setErrorMessage('something went wrong..');
     }
-  };
+  }
+  @action.bound setApiStatus(status) {
+    this.apiStatus = status;
+  }
+
+  @action.bound setErrorMessage(message) {
+    this.errorMessage = message;
+  }
 }
 
 export default User;
