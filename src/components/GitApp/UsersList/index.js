fdde4e4c5f34aca2845ed73bd645fb82ac290be5
M B S Yaswanth <mbs.yaswanth@gmail.com>
2019-08-22 12:14:32 +0530

    added picker for language

--- src/components/GitApp/UsersList/index.js
+++ src/components/GitApp/UsersList/index.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Picker} from 'react-native';
 import User from './User';
+import {Container, Language} from './styledComponents';
+
 class UserList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {language: 'en'};
+  }
+
   render() {
     return (
-      <View>
+      <Container>
+        <Language
+          selectedValue={this.state.language}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({language: itemValue})
+          }>
+          <Language.Item label="English" value="en" />
+          <Language.Item label="తెలుగు" value="tel" />
+          <Language.Item label="हिंदी" value="hin" />
+        </Language>
         <Text> User List </Text>
         <User />
-      </View>
+      </Container>
     );
   }
 }
